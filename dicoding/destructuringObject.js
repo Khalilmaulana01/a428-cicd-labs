3e11e65b29e411692005c7699593d8b7749b6fff
Prometheus Windows <khalilalkhabasan123@gmail.com>
2023-01-23 00:47:57 +0700

    Create dicoding, nadia-koding, & freecodecamp directory

---
+++ dicoding/destructuringObject.js
@@ -0,0 +1,24 @@
+//Destructuring Object ES6 is syntax to extract value from object and assign to variable
+//The Hard Way
+// const person = {
+// 	firstName: "John",
+// 	lastName: "Doe",
+// 	age: 18,
+// };
+
+// const firstName = person.firstName;
+// const lastName = person.lastName;
+// const age = person.age;
+
+// console.log(firstName, lastName, age);
+
+//The ES6 Way --> Destructuring Object (shorter way)
+const profile = {
+	firstName: "John",
+	lastName: "Doe",
+	age: 18,
+};
+
+//should be same name with object property
+const { firstName, lastName, age } = profile;
+console.log(firstName, lastName, age);
