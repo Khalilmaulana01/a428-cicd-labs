262002c41eaef4c634d4d115e573afb929cedc6b
Prometheus Windows <khalilalkhabasan123@gmail.com>
2023-01-23 01:39:50 +0700

    updating the change in dicoding folder

--- dicoding/destructuringArray.js
+++ dicoding/destructuringArray.js
@@ -1,4 +1,6 @@
 const favorites = ["Seafood", "Salad", "Nugget", "Soup"];
-const [firstFood, secondFood, thirdFood, fourthFood] = favorites;
+// const [firstFood, secondFood, thirdFood, fourthFood] = favorites;
+const [, , thirdFood, fourthFood] = favorites;
 
-console.log(firstFood);
+// console.log(firstFood); --> error because firstFood is not defined
+console.log(thirdFood, fourthFood);
